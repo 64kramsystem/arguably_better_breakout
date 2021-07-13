98f96efce6c3ac8925b233a77c1adbe00139f067
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-13 20:08:07 +0200

    Split app setup into plugins

--- src/main.rs
+++ src/main.rs
@@ -21,46 +21,62 @@ use wall::spawn_walls;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
+pub struct GameSetupPlugin;
+
+impl Plugin for GameSetupPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_plugins(DefaultPlugins)
+            .insert_resource(ClearColor(BACKGROUND_COLOR))
+            // This adds the Score resource with its default value of 0
+            .init_resource::<Score>()
+            // These systems run only once, before all other systems
+            .add_startup_system(spawn_cameras.system())
+            .add_startup_system(spawn_paddle.system())
+            .add_startup_system(spawn_ball.system())
+            .add_startup_system(spawn_walls.system())
+            .add_startup_system(spawn_bricks.system())
+            .add_startup_system(spawn_scoreboard.system());
+    }
+}
+
+pub struct InGamePlugin;
+
+impl Plugin for InGamePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(kinematics.system().label("kinematics"))
+            // We need to check for collisions before handling movement
+            // to reduce the risk of the ball passing through objects
+            .add_system(ball_collision.system().before("kinematics"))
+            // We need to handle input before we move our paddle,
+            // to ensure that we're responding to the most recent frame's events,
+            // avoiding input lag
+            // See https://github.com/bevyengine/bevy/blob/latest/examples/ecs/ecs_guide.rs
+            // for more information on system ordering
+            .add_system(
+                paddle_input
+                    .system()
+                    .before("bound_paddle")
+                    .before("kinematics"),
+            )
+            .add_system(
+                bound_paddle
+                    .system()
+                    .label("bound_paddle")
+                    // This system must run after kinematics, or the velocity will be set to 0
+                    // before the paddle moves, causing it to be stuck to the wall
+                    .after("kinematics"),
+            )
+            .add_system(update_scoreboard.system())
+            // Exits the game when `KeyCode::Esc` is pressed
+            // This is a simple built-in system
+            .add_system(exit_on_esc_system.system());
+    }
+}
+
 /// A simple implementation of the classic game "Breakout"
 fn main() {
     App::build()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(BACKGROUND_COLOR))
-        // This adds the Score resource with its default value of 0
-        .init_resource::<Score>()
-        // These systems run only once, before all other systems
-        .add_startup_system(spawn_cameras.system())
-        .add_startup_system(spawn_paddle.system())
-        .add_startup_system(spawn_ball.system())
-        .add_startup_system(spawn_walls.system())
-        .add_startup_system(spawn_bricks.system())
-        .add_startup_system(spawn_scoreboard.system())
-        .add_system(kinematics.system().label("kinematics"))
-        // We need to check for collisions before handling movement
-        // to reduce the risk of the ball passing through objects
-        .add_system(ball_collision.system().before("kinematics"))
-        // We need to handle input before we move our paddle,
-        // to ensure that we're responding to the most recent frame's events,
-        // avoiding input lag
-        // See https://github.com/bevyengine/bevy/blob/latest/examples/ecs/ecs_guide.rs
-        // for more information on system ordering
-        .add_system(
-            paddle_input
-                .system()
-                .before("bound_paddle")
-                .before("kinematics"),
-        )
-        .add_system(
-            bound_paddle
-                .system()
-                .label("bound_paddle")
-                // This system must run after kinematics, or the velocity will be set to 0
-                // before the paddle moves, causing it to be stuck to the wall
-                .after("kinematics"),
-        )
-        .add_system(update_scoreboard.system())
-        // Exits the game when `KeyCode::Esc` is pressed
-        // This is a simple built-in system
-        .add_system(exit_on_esc_system.system())
+        .add_plugin(GameSetupPlugin)
+        .add_plugin(InGamePlugin)
         .run();
 }
