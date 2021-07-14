229f4af92f171cefc04c19e9a6278e6e0dd31524
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-14 19:06:19 +0200

    Workaround kinematics issue with first system run (due to large time diff)

--- src/in_game/kinematics.rs
+++ src/in_game/kinematics.rs
@@ -8,10 +8,23 @@ use super::velocity::Velocity;
 /// Moves everything with both a Transform and a Velocity accordingly
 pub fn kinematics(
     mut last_time: Local<f64>,
+    mut post_first_invocation: Local<bool>,
     time: Res<Time>,
     query: Query<(&mut Transform, &Velocity)>,
 ) {
-    let last_time_diff = time.seconds_since_startup() - *last_time;
+    let mut last_time_diff = time.seconds_since_startup() - *last_time;
+
+    // When switching state, the first invocation will result in a large time diff, which causes issues.
+    //
+    // As of Jul/2021, this is a known issue (https://discord.com/channels/691052431525675048/742884593551802431/864911869461528657).
+    //
+    // > last_time starts at 0
+    // > which means if you use states, it wants to compensate for all of the time spent outside it
+    //
+    if !*post_first_invocation {
+        last_time_diff = last_time_diff.clamp(0., 0.333);
+        *post_first_invocation = true;
+    }
 
     query.for_each_mut(|(mut transform, velocity)| {
         transform.translation.x += velocity.x * last_time_diff as f32;
